56d71a47bc2743f7fab260e4df2751ba0b26cba4
Louise Lee <louiseleezhiwei@gmail.com>
2017-11-28 00:13:25 -0500

    Add sparse version due to space concerns

--- testaeig.m
+++ testaeig.m
@@ -6,15 +6,24 @@ opts.issym = 1;
 tic
     [V,et] = eigs(A,k,'lm',opts);
 toc
-tic
 b = randn(n,1);
-[W,e] = aeig(A,k,b); %note: change function signature in aeig.m to [V,b,e] to run this
+tic
+[W,e] = aeig(A,k,b);
 toc
-norm(sort(diag(et)) - sort(diag(eo)))
+tic
+[W2,e2] = sparse_aeig(A,k,b);
+toc
+norm(sort(diag(et)) - sort(diag(e)))
+norm(sort(diag(et)) - sort(diag(e2)))
 
 ortho = zeros(1,k);
+ortho2 = zeros(1,k);
 for i = 1:k
     v = (A-e(i,i)*eye(n,n))*W(:,i);
     ortho(i) = v'*A*A*b;
+    
+    v2 = (A-e(i,i)*eye(n,n))*W2(:,i);
+    ortho2(i) = v2'*A*A*b;
 end
 ortho
+ortho2
