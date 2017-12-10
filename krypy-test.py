6962aa3e7760ad5a0e2bd0368b2d08408d9f5c9d
Louise Lee <louiseleezhiwei@gmail.com>
2017-12-10 16:50:47 -0500

    Clean up krypy-test

--- krypy-test.py
+++ krypy-test.py
@@ -4,55 +4,38 @@ import numpy
 from krypy.linsys import LinearSystem, Gmres, Cg
 from krypy.deflation import DeflatedCg, DeflatedGmres, Ritz
 from krypy.utils import Arnoldi, ritz
-
-#A=numpy.diag([1e-3]+list(range(2, 101)))
 from scipy import random, linalg
+
+def find_deflation_subspace(A,b,k,ortho='dmgs',ritz_type='ritz'):
+    Ar = Arnoldi(A,b,ortho=ortho)
+    for i in range(1,k+1):
+        Ar.advance()
+    [V,H] = Ar.get()
+    [theta,U,resnorm,Z] = ritz(H,V,type=ritz_type)
+    return Z
+
 matrixSize = 100
 R = random.rand(matrixSize,matrixSize)
 A = numpy.dot(R,R.transpose())
-
 b=numpy.ones((matrixSize, 1))
 linear_system = LinearSystem(A=A,b=b,self_adjoint=True,positive_definite=True)
 
 ts = time.time()
-sol2 = Cg(linear_system,maxiter=1000)
+cg_sol = Cg(linear_system,maxiter=1000)
 te = time.time()
-print((te-ts)*1000)
-#sol3 = Gmres(linear_system)
+print("CG time taken: ", (te-ts)*1000)
+
+ts = time.time()
+deflated_sol = DeflatedCg(linear_system,U=find_deflation_subspace(A,b,10),maxiter=1000)
+te = time.time()
+print("Deflated CG time taken: ", (te-ts)*1000)
 
 # plot residuals
 from matplotlib import pyplot
-
-# use tex
-#from matplotlib import rc
-#rc('text', usetex=True)
-
-# use beautiful style
-#from mpltools import style
-#style.use('ggplot')
-
 pyplot.figure(figsize=(6, 4), dpi=100)
 pyplot.xlabel('Iteration $i$')
 pyplot.ylabel(r'Relative residual norm $\frac{\|r_i\|}{\|b\|}$')
-#pyplot.semilogy(sol.resnorms)
-pyplot.semilogy(sol2.resnorms,label='cg')
-#pyplot.semilogy(sol3.resnorms)
-
-#pyplot.savefig('example.png', bbox_inches='tight')
-
-
-ts = time.time()
-Ar = Arnoldi(A,b)
-for i in range(1,11):
-    Ar.advance()
-[V,H] = Ar.get()
-[theta,U,resnorm,Z] = ritz(H,V)
-#sol = DeflatedGmres(linear_system,maxiter=1000,store_arnoldi=True,tol=1e-1)
-#U = Ritz(sol, mode='harmonic')
-sol4 = DeflatedCg(linear_system,U=Z,maxiter=1000)
-#sol4 = DeflatedCg(linear_system,U=U.get_vectors(),maxiter=1000)
-te = time.time()
-print((te-ts)*1000)
-pyplot.semilogy(sol4.resnorms,label='deflated')
+pyplot.semilogy(cg_sol.resnorms,label='cg')
+pyplot.semilogy(deflated_sol.resnorms,label='deflated')
 pyplot.legend()
 pyplot.show()
